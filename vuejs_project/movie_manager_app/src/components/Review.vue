5ef651ac233113b4e5edfdb2238cb30b4e548429
thomas_soutif <thomas.soutif@nuabee.fr>
2022-11-06 17:59:38 +0100

    `HomeView` and `Review` components
    Display the list of review on the home page

---
+++ vuejs_project/movie_manager_app/src/components/Review.vue
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+<template>
+
+<div>
+
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>Moovie Name here{{moovie}}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+
+  <v-list-item two-line>
+    <v-list-item-content>
+      <v-list-item-title>Grade</v-list-item-title>
+      <v-list-item-subtitle>{{ grade }}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+
+
+
+
+</div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: "Review",
+  props:['moovie','grade'],
+  data: () => ({
+
+  }),
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- vuejs_project/movie_manager_app/src/views/HomeView.vue
+++ vuejs_project/movie_manager_app/src/views/HomeView.vue
@@ -1,15 +1,41 @@
 <template>
-  <hello-world />
-</template>
+<div>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+    tile
+     v-for="review in reviews"
+  >
+    <v-list-item link>
+      <Review :grade="review.grade" :moovie="review.moovie"></Review>
+    </v-list-item>
 
+  </v-card>
+
+</div>
+
+</template>
+import axios from 'axios';
 <script>
   import HelloWorld from '../components/HelloWorld'
+  import MoviesReviewService from "@/services/MoviesReviewService";
+  import axios from "axios";
+  import Review from "@/components/Review";
 
   export default {
     name: 'Home',
-
+    data: () => ({
+      reviews: [],
+    }),
     components: {
-      HelloWorld,
+      Review
     },
+    async mounted() {
+      try{
+        this.reviews = await MoviesReviewService.getAllReviews();
+        console.log(this.reviews)
+      }catch (err){
+      }
+    }
   }
 </script>
