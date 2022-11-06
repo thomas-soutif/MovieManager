cdf35dfbe256d9228f75790415b0db24f5d9b69c
thomas_soutif <thomas.soutif@nuabee.fr>
2022-11-06 18:44:50 +0100

    Add Pagination config to DRF

--- django_project/src/movie_app/settings.py
+++ django_project/src/movie_app/settings.py
@@ -91,6 +91,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
