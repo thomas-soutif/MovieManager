0d1fbe852d0f654c4ac1410bdeb7f90fbfbf6eff
thomas_soutif <thomas.soutif@nuabee.fr>
2022-11-07 13:33:41 +0100

    System of pagination for home page
    Add the pagination for the reviews , and improved in same time the
    service to decharge most at possible the .Vue file from managing the
    pagination

--- vuejs_project/movie_manager_app/src/services/MoviesReviewService.js
+++ vuejs_project/movie_manager_app/src/services/MoviesReviewService.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import HOST from "../../services.config";
-const url = HOST + 'api/reviews';
 
 class ReviewService{
-
-    static getAllReviews(){
-
+    constructor(limit) {
+        self.url = HOST + 'api/reviews';
+        self.limit = limit;
+    }
+    getAllReviews(offset){
         return new Promise(async (resolve, reject) => {
             try {
-                const res = await axios.get(url)
-                resolve(res.data)
+                const res = await axios.get(self.url + "?limit=" + self.limit + "&offset=" + offset);
+
+                resolve(res.data.results)
             }catch (err){
                 reject(err)
             }
--- vuejs_project/movie_manager_app/src/views/HomeView.vue
+++ vuejs_project/movie_manager_app/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
   <v-card
+      v-if="reviews.length"
     class="mx-auto"
     max-width="400"
     tile
@@ -11,7 +12,10 @@
     </v-list-item>
 
   </v-card>
-
+  <v-pagination
+      v-model="page"
+      :length="6"
+    ></v-pagination>
 </div>
 
 </template>
@@ -26,16 +30,35 @@ import axios from 'axios';
     name: 'Home',
     data: () => ({
       reviews: [],
+      limit: 1,
+      movieReviewService: new MoviesReviewService(1),
+      page: 1,
     }),
     components: {
       Review
     },
     async mounted() {
       try{
-        this.reviews = await MoviesReviewService.getAllReviews();
+        this.reviews = await this.movieReviewService.getAllReviews(this.getOffSet());
         console.log(this.reviews)
       }catch (err){
+        console.log(err);
+      }
+    },
+    methods: {
+      getOffSet : function (){
+        return (this.page - 1) * this.limit;
+      },
+    },
+    watch :{
+      page : function (){
+        console.log("hey")
+        this.movieReviewService.getAllReviews(this.getOffSet()).then((reviews) => {
+          console.log(reviews)
+          this.reviews = reviews;
+        });
       }
     }
+
   }
 </script>
