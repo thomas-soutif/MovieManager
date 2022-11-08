9d91551c46c9ba42371595f6c27343ac4c92373b
thomas_soutif <thomas.soutif@nuabee.fr>
2022-11-08 09:46:43 +0100

    Related name to Review
    Movie model can now access all his reviews

--- django_project/src/movie_app/models.py
+++ django_project/src/movie_app/models.py
@@ -5,11 +5,13 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=45)
     description = models.TextField(help_text="Enter a brief description of the movie")
     actors = models.ManyToManyField(Actor, related_name="movies")
 
+
 class Review(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     grade = models.IntegerField(help_text="Enter a grade between 1 and 5")
